9cc54d471395ec9e9514eed27149ee6d2dd58fee
Alaikum <carangiadamo@gmail.com>
2022-10-11 15:49:49 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('metaTitle','Homa Page')
+
+@section('content')
+<h1>Homepage</h1>
+{{-- {{dd($movies)}} --}}
+{{-- @foreach ($movies as $item)
+    <h2>
+        {{$item->title}}
+    </h2>
+@endforeach --}}
+
+@endsection
